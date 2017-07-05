8423c4ebbbb7f801193e1026f1a2ead8a79b16b2
alxsnchez <alxsnchez@gmail.com>
2017-07-05 21:15:26 +0200

    Implemented CollapsingToolbarLayout

--- app/src/main/java/com/alxsnchez/mymovies/DetailActivity.java
+++ app/src/main/java/com/alxsnchez/mymovies/DetailActivity.java
@@ -2,9 +2,11 @@ package com.alxsnchez.mymovies;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -31,17 +33,15 @@ public class DetailActivity extends AppCompatActivity {
 
     private final static String BASE_URL = "https://api.themoviedb.org/3/movie";
     private final static String API_PARAM = "api_key";
-    private final static String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w185/";
+    private final static String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w500/";
 
-    private RelativeLayout mContainerLayout;
+    private NestedScrollView mContainerLayout;
 
-    private TextView mTitleTextView;
     private ImageView mPosterImageView;
     private TextView mDateTextView;
     private TextView mScoreTextView;
     private TextView mOverviewTextView;
-    private ProgressBar mDetailProgressBar;
-    private TextView mErrorMessage;
+    private CollapsingToolbarLayout mCollapsingToolbar;
 
     private String movieTitle;
     private String posterPath;
@@ -54,19 +54,23 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        mContainerLayout = (RelativeLayout) findViewById(R.id.container_layout);
+        mContainerLayout = (NestedScrollView) findViewById(R.id.scroll);
 
-        mTitleTextView = (TextView) findViewById(R.id.tv_movie_title);
+        //mTitleTextView = (TextView) findViewById(R.id.tv_movie_title);
         mPosterImageView = (ImageView) findViewById(R.id.iv_poster_detail);
         mDateTextView = (TextView) findViewById(R.id.tv_movie_date);
         mScoreTextView = (TextView) findViewById(R.id.tv_movie_score);
         mOverviewTextView = (TextView) findViewById(R.id.tv_movie_overview);
-        mDetailProgressBar = (ProgressBar) findViewById(R.id.detail_progress_bar);
-        mErrorMessage = (TextView) findViewById(R.id.tv_error_message);
+        mCollapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         Intent intent = getIntent();
         if (intent.hasExtra("MOVIE_ID")){
-            hideInformation();
             String movieId = intent.getStringExtra("MOVIE_ID");
             requestInformation(movieId);
 
@@ -121,15 +125,13 @@ public class DetailActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         if (errorStatus){
-                            mDetailProgressBar.setVisibility(View.INVISIBLE);
-                            mErrorMessage.setText(R.string.error_message);
+                            mCollapsingToolbar.setTitle(getString(R.string.error_message));
                         } else {
-                            mTitleTextView.setText(movieTitle);
+                            mCollapsingToolbar.setTitle(movieTitle);
                             Picasso.with(DetailActivity.this).load(BASE_POSTER_URL + posterPath).error(R.drawable.empty_thumbnail).into(mPosterImageView);
                             mDateTextView.setText(calculateYear(releaseDate));
                             mScoreTextView.setText(String.format(Locale.getDefault(), "%.1f/10", voteAverage));
                             mOverviewTextView.setText(movieOverview);
-                            showInformation();
                         }
 
                     }
@@ -141,16 +143,6 @@ public class DetailActivity extends AppCompatActivity {
     private String calculateYear(String date){
         List<String> dateArray = Arrays.asList(date.split("-"));
         return dateArray.get(0);
-    }
-
-    private void showInformation(){
-        mContainerLayout.setVisibility(View.VISIBLE);
-        mDetailProgressBar.setVisibility(View.INVISIBLE);
-    }
-
-    private void hideInformation(){
-        mDetailProgressBar.setVisibility(View.VISIBLE);
-        mContainerLayout.setVisibility(View.GONE);
     }
 
 }
